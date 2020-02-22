bbdeda35e0ef88a8d1a66b35693fd9e7575791c0
Andrew Keely <andrew.keely@gmail.com>
2020-02-21 23:51:42 -0500

    Actuator configuration

--- src/main/java/io/squant/app/SecurityConfig.java
+++ src/main/java/io/squant/app/SecurityConfig.java
@@ -1,5 +1,6 @@
 package io.squant.app;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,7 +10,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http.authorizeRequests()
+         .antMatchers("/actuator/*").permitAll()
          .anyRequest().authenticated()
          .and()
          .oauth2Login();
